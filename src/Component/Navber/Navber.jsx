3a5465604b9ea41cac6dae94503200fc394ce95f
reshad21 <miakala7@gmail.com>
2024-02-10 17:14:07 +0600

    navber width change

--- src/Component/Navber/Navber.jsx
+++ src/Component/Navber/Navber.jsx
@@ -8,8 +8,8 @@ const Navber = () => {
     }
     return (
         <div>
-            <div className='bg-[#102027] sticky top-0 left-0 bottom-0 right-0 z-40 clear-both'>
-                <ul className='flex md:gap-0 justify-between items-center mx-auto max-w-6xl'>
+            <div className='bg-[#102027] sticky top-0 left-0 bottom-0 right-0 z-40 mx-auto max-w-6xl'>
+                <ul className='flex md:gap-0 justify-between items-center'>
                     <li className='grow'>
                         <span className='text-center cursor-pointer py-3 px-4 block text-base md:text-xl font-normal text-white hover:bg-[#29434e]'>Introduction</span>
                     </li>
